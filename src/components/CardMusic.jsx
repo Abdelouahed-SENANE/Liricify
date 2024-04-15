61456006ebabb976ae8469cbc3ae3694a45975cf
senane <abdosenane75@gmail.com>
2024-04-15 14:08:01 +0100

    Added CardMusic

--- src/components/CardMusic.jsx
+++ src/components/CardMusic.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
-
-const CardMusic = () => {
+import { FaPlay } from "react-icons/fa";
+import { MdAlbum } from "react-icons/md";
+import {Link } from 'react-router-dom'
+const CardMusic = ({object}) => {
   return (
-    <div>CardMusic</div>
+    <div className='border border-slate-300 rounded-md p-4'>
+        <h1 className='text-lg bold'>Title Music</h1>
+        <div className='flex gap-1 items-center'>
+          <FaPlay/>
+          <span>Track: hello</span>
+        </div>
+        <div className='flex gap-1 items-center'>
+          <MdAlbum/>
+          <span>Album: hello</span>
+        </div>
+        <div className='my-1'>
+          <Link to={'/details'} className='block w-full text-center bg-slate-800 text-white py-2'>View Lyrics</Link>
+        </div>
+    </div>
   )
 }
 
--- src/components/WrapperMusic.jsx
+++ src/components/WrapperMusic.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
 
-const WrapperMusic = () => {
-  return (
-    <div>WrapperMusic</div>
-  )
-}
+const WrapperMusic = ({ children }) => {
+    return (
+        <div className="container  mt-10 mx-auto w-[75%]">
+            <header  className="text-4xl font-medium text-center">All Tracks</header>
+            <div className="grid grid-cols-3 gap-5">{children}</div>
+        </div>
+    );
+};
 
-export default WrapperMusic
+export default WrapperMusic;
