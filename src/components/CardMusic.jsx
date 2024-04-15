7c7daf0d5a77a2ca9080e5775c557e02250ca748
senane <abdosenane75@gmail.com>
2024-04-15 11:26:17 +0100

    add new feautures

---
+++ src/components/CardMusic.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CardMusic = () => {
+  return (
+    <div>CardMusic</div>
+  )
+}
+
+export default CardMusic
---
+++ src/components/WrapperMusic.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const WrapperMusic = () => {
+  return (
+    <div>WrapperMusic</div>
+  )
+}
+
+export default WrapperMusic
