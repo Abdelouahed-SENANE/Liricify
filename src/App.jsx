aded3b318d074edfe5cb9b66f6dc369fdaf5e2a7
senane <abdosenane75@gmail.com>
2024-04-15 11:22:01 +0100

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Details from './pages/Details'
 
 
 function App() {
 
   return (
     <>
-      
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/details' element={<Details/>}/>
+        </Routes>
+      </Router>
     </>
   )
 }
